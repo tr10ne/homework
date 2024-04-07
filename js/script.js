4dab275cd1069a9df840da062d69f8851adb296f
tr10ne <tr1one@ya.ru>
2024-04-07 20:59:29 +0300

    JS Homework Array Finish

--- js/script.js
+++ js/script.js
@@ -1,140 +1,92 @@
-const btn = document.querySelector('.btn');
+//ЗАДАЧА 1 - Создание массива
+const myArr1 = [7, 'John', {car: "Lada"}];
+console.log(`Решение задачи #1:\nМассив:${myArr1}\nПервый элемент массива: ${myArr1[0]}\nДлина массива: ${myArr1.length}`);
 
-btn.addEventListener('click',() => {
-//Объявляю переменные
-    let task, question;
+//ЗАДАЧА 2 - Замена элементов массива
+const myArr2 = [7, 'John', {car: "Lada"}];
+const newMyArr2 = [...myArr2];
+newMyArr2[1] = 'Trivon';
+console.log(`Решение задачи #2:\nНачальный массив:${myArr2}\nИзмененный массив: ${newMyArr2}`);
 
-//Цикл для выбора задания
-    do {
-        task = prompt('Введите номер задания, которое хотите проверить', 'от 1 до 7');
-    } while (isNaN(task) || (task < 1 || task > 7) && task !== null);
+//ЗАДАЧА 4 - Добавление элементов в массив
+const myArr4 = [7, 'John', {car: "Lada"}];
+const newMyArr4 = [...myArr4];
+newMyArr4.push(4, 5, 6);
+console.log(`Решение задачи #4:\nНачальный массив:${myArr4}\nИзмененный массив: ${newMyArr4}`);
 
-//Условие для того чтобы, если цикл прекратится отменой, код ниже не выполнялся
-    if (!isNaN(task) && (task >= 1 && task <= 7) && task !== null) {
-        
-    //Создаю вспомогательную функцию чтобы не дублировать одинаковые куски кода
-        function myQuestion() {
-            question = prompt(`Чтобы закончить введите любой символ или нажмите "Отмена"\n ------------------------------------------------------\n Чтобы проверить следующее задание нажмите "Продолжить"`);
-            if (question !== '' || question === null) {
-            
-            //этот момент загуглил
-                throw new Error('До свидания!')
-            } else {
-            }
+//ЗАДАЧА 5 - Добавление элементов в начало массива
+const myArr5 = [true, null];
+const newMyArr5 = [...myArr5];
+newMyArr5.unshift('Привет', 100);
+console.log(`Решение задачи #5:\nНачальный массив:${myArr5}\nИзмененный массив: ${newMyArr5}\nДлина измененного массива: ${newMyArr5.length}`);
+
+//ЗАДАЧА 6 - Добавление элемента по определенному индексу
+const myArr6 = [1, 2];
+const newMyArr6 = [...myArr6];
+newMyArr6[10] = 'abc';
+console.log(`Решение задачи #6:\nНачальный массив:${myArr6}\nИзмененный массив: ${newMyArr6}\nДлина измененного массива: ${newMyArr6.length}\nДлина массива 11, так как добавляются все ячейки с пустыми элементами с 0-го до 10-го`);
+
+//ЗАДАЧА 7 - Массив объектов
+const myArr7 = [
+    {carBrand: 'Kia',
+    price: 3000000,
+    isAvailableForSale: true},
+    {carBrand: 'Audi',
+    price: 6000000,
+    isAvailableForSale: false},
+    {carBrand: 'Opel',
+    price: 3500000,
+    isAvailableForSale: true}
+];
+const newMyArr7 = [...myArr7];
+newMyArr7.push({carBrand: 'Lada',
+price: 1500000,
+isAvailableForSale: true});
+console.log(`Решение задачи #7:\nНачальный массив:${JSON.stringify(myArr7)}\nИзмененный массив: ${JSON.stringify(newMyArr7)}`);
+
+//ЗАДАЧА 8 - Случайные числа
+const myNumbers = [5, 7, 3, 1, 2];
+const newMyNumbers = [...myNumbers]
+const randomNum = () => Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+const addArr = (num) => newMyNumbers.push(num);
+//на самом деле в процессе реализации я забыл про includes, он бы подошел здесь больше, но так как я уже начал делать таким способом решил довести до конца
+do {
+    let myRandom = randomNum();
+    let check = null;
+    for (let key of myNumbers) {
+        if (myRandom === key) {
+            check = 'no ok'
         }
-
-    //ЗАДАЧА 1 - Проверка объекта
-        const isEmpty = obj => {for (let key in obj) {return false} return true};
-    
-    //ЗАДАЧА 2 - Операторы "rest"  и "spread"
-        const meanScore = (...num) => {if (num.length === 0) return 'Аргументов не задано'; let sum = 0; for (let i of num) {sum += i;}
-        const average = sum / num.length;
-        return average.toFixed(2);
-        }
-
-    //ЗАДАЧА 3 - Минимальный и максимальный параметр
-        const minMax = (...num) => {
-            if (num.length === 0 || num.length === 1) return 'Аргументов не задано, либо задан только 1 аргумент';
-            let min = num[0];
-            let max = num[0];
-            for (let i in num) {
-                if (min < num[i]) {
-                    min = min
-                } else {
-                    min = num[i]
-                }
-                if (max > num[i]) {
-                    max = max
-                } else {
-                    max = num[i]
-                }
-            }
-            const minMaxObj = {
-                'Минимальное значение': min,
-                'Максимальное значение': max
-            }
-            return minMaxObj;
-        }
-    
-    //ЗАДАЧА 4 - Spread Operator
-        const person = {
-            firstName: 'John',
-            lastName: 'Trivon'
-        },
-            car = {
-                marka: 'Peugeot',
-                model: '3008',
-                year: 2011  
-            }
-        const personCar = {...person, ...car};
-
-    //ЗАДАЧА 5 - Копия объекта
-        const cars = {
-                marka: 'Peugeot',
-                model: '3008',
-                year: 2011  
-        },
-            myObj = {...cars};
-
-    //ЗАДАЧА 6 - Копия объекта2
-        const persons = {
-            firstName: 'John',
-            lastName: 'Trivon',
-            car: {
-                marka: 'Peugeot',
-                model: '3008',
-                year: 2011 
-            }
-        },
-            newObj = JSON.parse(JSON.stringify(persons))
-
-    //ЗАДАЧА 7 - Конвертация JSON <=> объекты
-        const data = {
-            firstName: 'John',
-            lastName: 'Trivon',
-            age: 33,
-            address: {
-                city: 'Tula',
-                street: 'Vostochnaya',
-                numHouse: 13
-            }
-        }
-        const jsonData = JSON.stringify(data);
-        const parsedData = JSON.parse(jsonData);
-
-    //Мои инициативы
-    task = +task;
-    switch (task) {
-        case 1: 
-            let schedule = {};
-            alert(`Решение Задачи #1:\n\nДля пустого объекта - ${isEmpty(schedule)}`);
-            schedule["8:30"] = "get up";
-            alert(`Решение Задачи #1:\n\nДля не пустого объекта - ${isEmpty(schedule)}`);
-            myQuestion();
-
-        case 2:
-            alert(`Решение задачи #2:\n\nСреднее значение для пустого набора символов - ${meanScore()}\nСреднее значение для набора символов 3, 7, 6 - ${meanScore(3, 7, 6)}\nСреднее значение для набора символов 10, 20, 30, 40, 50 - ${meanScore(10, 20, 30, 40, 50)}`);
-            myQuestion();
-
-        case 3:
-            alert(`Решение задачи #3:\n\nДля набора чисел 4, 2, 7, 5, 9 - ${JSON.stringify(minMax(4,2,7,5,9))}`);
-            myQuestion();
-
-        case 4:
-            alert(`Решение задачи #4:\n\nПервый объект - ${JSON.stringify(person)}\n\nВторой объект - ${JSON.stringify(car)}\nОбъединение объектов - ${JSON.stringify(personCar)}`);
-            myQuestion();
-
-        case 5:
-            alert(`Решение задачи #5:\n\nОбъект - ${JSON.stringify(cars)}\n\nКопия объекта - ${JSON.stringify(myObj)}`);
-            myQuestion();
-
-        case 6:
-            alert(`Решение задачи #6:\n\nОбъект - ${JSON.stringify(persons)}\n\nКопия объекта - ${JSON.stringify(newObj)}`);
-            myQuestion();
-
-        case 7:
-            alert(`Решение Задачи #7:\n\nОбъект - ${JSON.stringify(data)}\n\nОбъект в JSON - ${jsonData}\n\nКопия объекта из JSON - ${JSON.stringify(parsedData)}`);
     }
-    } else {
+    if (check === null) {
+        addArr(myRandom)
     }
-})
+} while (myNumbers.length === newMyNumbers.length)
+console.log(`Решение задачи #8:\nНачальный массив: ${myNumbers}\nИзмененный массив: ${newMyNumbers}`);
+
+//ЗАДАЧА 9 - Перебор элементов массива
+const myCities = ['London', 'New York', 'Singapore'];
+const newMyCities = [...myCities];
+function cityInfo(city, index, arrayName) {
+    return `${city} is at the ${+index + 1} position in the ${arrayName} array\n`;
+}
+let cityInfoReturn = '';
+for (let i in myCities) {
+    cityInfoReturn = cityInfoReturn + cityInfo(myCities[i], i, 'myCities');
+}
+console.log(`Решение задачи #9:\n${cityInfoReturn}`);
+
+//ЗАДАЧА 10 - Поиск объектов в массиве
+const posts = [
+  { postId: 1355, commentsQuantity: 5 },
+  { postId: 5131, commentsQuantity: 13 },
+  { postId: 6134, commentsQuantity: 2 },
+]
+let findPostById = (id, massive) => {for (let key of massive) {
+    if (key.postId === id) {
+        return key;
+        }
+    }
+    return undefined;
+}
+console.log(`Решение задачи #10:\nПример для существующего ID - ${JSON.stringify(findPostById(5131, posts))}\nПример для не существующего ID - ${JSON.stringify(findPostById(666, posts))}`);
